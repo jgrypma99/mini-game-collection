43f9f73d9eb9c27087b42d31963e3d6808676a51
jgrypma99 <jgrypma99@gmail.com>
2024-11-17 18:05:26 -0500

    Added a simple scene and finished up a few things plus playtesting

--- unity-project/mini-game-collection/Assets/Scripts/Prisoner's Dillema/endgame_manager.cs
+++ unity-project/mini-game-collection/Assets/Scripts/Prisoner's Dillema/endgame_manager.cs
@@ -15,6 +15,22 @@ public class endgame_manager : MonoBehaviour
     bool p2Win;
     bool tie;
     bool loss;
+    bool showEndMessage = true;
+
+    //Endings
+    public GameObject endState;
+    public GameObject lossText;
+    public GameObject tieText;
+    public GameObject p1WinText;
+    public GameObject p2WinText;
+
+    private void Start()
+    {
+        lossText.SetActive(false);
+        tieText.SetActive(false);
+        p1WinText.SetActive(false);
+        p2WinText.SetActive(false);
+    }
 
     // Update is called once per frame
     /* Actions
@@ -35,30 +51,47 @@ public class endgame_manager : MonoBehaviour
         }
         //once timer hits zero enter the endgame state
         if (gameTimer == 0)
-        {
-            if (Input.GetButtonDown("P1_Action1") && Input.GetButtonDown("P2_Action1"))
+        {   
+            if (showEndMessage == true)
             {
+                endState.SetActive(true);
+            }
+            if (showEndMessage == false)
+            {
+                endState.SetActive(false);
+            }
+
+            if (Input.GetButtonDown("P1_Action1") && Input.GetButtonDown("P2_Action1"))
+            {               
                 loss = true;
                 Debug.Log("Both lose");
                 //Loss event
+                lossText.SetActive(true);
+                showEndMessage = false;
             }
             else if (Input.GetButtonDown("P1_Action2") && Input.GetButtonDown("P2_Action2"))
             {
                 tie = true;
                 //Tie event
                 Debug.Log("Both tie");
+                tieText.SetActive(true);
+                showEndMessage = false;
             }
             else if (Input.GetButtonDown("P1_Action1") && Input.GetButtonDown("P2_Action2"))
             {
                 p1Win = true;
                 //p1Win event
                 Debug.Log("P1 Win");
+                p1WinText.SetActive(true);
+                showEndMessage = false;
             }
             else if (Input.GetButtonDown("P1_Action2") && Input.GetButtonDown("P2_Action1"))
             {
                 p2Win = true;
                 //p2Win event
                 Debug.Log("P2 Win");
+                p2WinText.SetActive(true);
+                showEndMessage = false;
             }
         }
     }
--- unity-project/mini-game-collection/Assets/Scripts/Prisoner's Dillema/global_timer.cs
+++ unity-project/mini-game-collection/Assets/Scripts/Prisoner's Dillema/global_timer.cs
@@ -6,18 +6,38 @@ using TMPro;
 public class global_timer : MonoBehaviour
 {
     public TextMeshProUGUI timerText;
+    public GameObject timer;
+    public GameObject rules;
     public float timeRemaining = 30f;
-    // Update is called once per frame
-    void Update()
+    bool startState;
+
+    private void Start()
     {
-        while (timeRemaining > 0)
+        startState = true;
+        timer.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetButtonDown("P1_Action1") || Input.GetButtonDown("P2_Action1") || Input.GetButtonDown("P1_Action2") || Input.GetButtonDown("P2_Action2"))
         {
-            timeRemaining -= Time.deltaTime;
-            timerText.text = timeRemaining.ToString();
+            startState = false;
+            timer.SetActive(true);
+        }
+
+        if (startState == false)
+        {
+            rules.SetActive(false);
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.fixedDeltaTime;
+                timerText.text = timeRemaining.ToString();               
+            }
             //if timer ever goes below zero for whatever reason set it to zero
             if (timeRemaining < 0)
-            { 
-                timeRemaining = 0; 
+            {
+                timeRemaining = 0;
             }
         }
     }
